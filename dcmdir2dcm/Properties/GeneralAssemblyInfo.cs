6143c3acd916d09266decb0f63c734926cd12d69
TomikT <tauer.t@windowslive.com>
2016-01-01 17:10:16 +0100

    Added Fody costura weavers + updated assembly infos

---
+++ dcmdir2dcm/Properties/GeneralAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Provides easy way to converts several dicom image files into single multiframe one")]
+[assembly: AssemblyCompany("Tauco")]
+[assembly: AssemblyProduct("dcmdir2dcm")]
+[assembly: AssemblyCopyright("Copyright © Tauco 2015")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.*")]
